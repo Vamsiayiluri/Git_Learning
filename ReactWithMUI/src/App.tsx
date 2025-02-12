ddeaeabccc5e464e8a6b5c749747e0750ff2c825
Vamsiayiluri <vamsi.ayiluri@cognine.com>
2025-02-12 11:05:31 +0530

    Added changes

---
+++ ReactWithMUI/src/App.tsx
@@ -0,0 +1,34 @@
+import { MuiButton } from "./components/MuiButton";
+import { MuiCheckBox } from "./components/MuiCheckbox";
+// import { MuiRadioButton } from "./components/MuiRadioButton";
+import { MuiRating } from "./components/MuiRating";
+// import { MuiSelect } from "./components/MuiSelect";
+// import { MuiSwitch } from "./components/MuiSwitch";
+// import { MuiTextField } from "./components/MuiTextField";
+// import { MuiTypography } from "./components/MuiTypography";
+
+import MuiAutocomplete from "./components/MuiAutocomplete";
+import { MuiLayout } from "./components/MuiLayout";
+import MuiFab from "./components/MuiFab";
+import MuiSlider from "./components/MuiSlider";
+
+function App() {
+  return (
+    <>
+      {/* <MuiTypography></MuiTypography> */}
+      {/* <MuiButton></MuiButton> */}
+      {/* <MuiTextField></MuiTextField> */}
+      {/* <MuiSelect></MuiSelect> */}
+      {/* <MuiRadioButton></MuiRadioButton> */}
+      {/* <MuiCheckBox></MuiCheckBox> */}
+      {/* <MuiSwitch></MuiSwitch> */}
+      {/* <MuiRating></MuiRating> */}
+      {/* <MuiLayout></MuiLayout> */}
+      {/* <MuiAutocomplete></MuiAutocomplete> */}
+      {/* <MuiFab></MuiFab> */}
+      <MuiSlider></MuiSlider>
+    </>
+  );
+}
+
+export default App;
